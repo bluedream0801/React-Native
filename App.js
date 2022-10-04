8aa493c2a8889fe4adf1f590dd4a14eef6dbaf51
Blue Dream <bluedream0801@gmail.com>
2022-10-04 16:26:09 +0800

    I added 8 files

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, Image, View, TouchableOpacity,TextInput } from 'react-native'
+import Home from './screens/Home'
+class App extends React.Component {
+  render() {
+    return (
+      <Home/>
+      
+    )
+  }
+}
+export default App
